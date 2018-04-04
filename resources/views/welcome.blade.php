719a0788c3d1401a036ed2b655cc440490f98628
waghanna <wagdy.hanna@gmail.com>
2018-04-03 23:30:05 -0400

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -33,12 +33,16 @@
             .title {
                 font-size: 96px;
             }
+            .title2 {
+                font-size: 48px;
+            }
         </style>
     </head>
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title">Alert Systems International</div>
+                <div class="title">Website is under construction, please visit us again soon! If you have any questions please call us at 978-955-8442.</div>
             </div>
         </div>
     </body>
