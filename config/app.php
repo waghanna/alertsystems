8fdcf4326fbaa5911e370ceb261f5da2437d5e70
waghanna <wagdy.hanna@gmail.com>
2018-04-24 20:00:17 -0400

    Update app.php

--- config/app.php
+++ config/app.php
@@ -157,6 +157,7 @@ return [
         App\Providers\RouteServiceProvider::class,
         
         //Collective\Html\HtmlServiceProvider::class,
+        Collective\Html\HtmlServiceProvider::class,
 
     ],
 
